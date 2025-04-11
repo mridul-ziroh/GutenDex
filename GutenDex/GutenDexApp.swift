bd700492adf7455230a300706eb1f61d09f82791
mridul-ziroh <mridul.pschauhan@ziroh.com>
2025-04-11 19:18:58 +0530

    Initial Commit

---
+++ GutenDex/GutenDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  GutenDexApp.swift
+//  GutenDex
+//
+//  Created by mridul-ziroh on 11/04/25.
+//
+
+import SwiftUI
+
+@main
+struct GutenDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
